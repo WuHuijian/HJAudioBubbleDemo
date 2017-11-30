bc745d852945d9f591518f72d64ad32d0bb385d6
WHJ <760503653@qq.com>
2017-11-30 15:40:10 +0800

    Initial Commit

---
+++ HJAudioBubbleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HJAudioBubbleDemo
+//
+//  Created by WHJ on 2017/11/30.
+//  Copyright © 2017年 WHJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HJAudioBubbleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HJAudioBubbleDemo
+//
+//  Created by WHJ on 2017/11/30.
+//  Copyright © 2017年 WHJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
